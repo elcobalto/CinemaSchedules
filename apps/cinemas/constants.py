ca6e1e5ed2f2f4bbbd9befb430f19e1b94c082a1
Cristóbal Sepúlveda <crisep95@gmail.com>
2022-04-13 11:56:42 -0400

    feat: add cinemark scrapper (#2)

---
+++ apps/cinemas/constants.py
@@ -0,0 +1,103 @@
+CINEHOYTS_VALPARAISO_URL = (
+    "https://cinehoyts.cl/cartelera/norte-y-centro-de-chile/valparaiso"
+)
+CINEHOYTS_ARAUCO_ESTACION_URL = (
+    "https://cinehoyts.cl/cartelera/santiago-centro/arauco-estacion"
+)
+CINEPOLIS_LA_REINA_URL = (
+    "https://cinehoyts.cl/cartelera/santiago-oriente/cinepolis-la-reina"
+)
+CINEHOYTS_ARAUCO_MAIPU_URL = (
+    "https://cinehoyts.cl/cartelera/santiago-poniente-y-norte/arauco-maipu"
+)
+CINEHOYTS_PUENTE_ALTO_URL = (
+    "https://cinehoyts.cl/cartelera/santiago-sur/cinepolis-espacio-urbano-puente-alto"
+)
+CINEHOYTS_CHILLAN_URL = "https://cinehoyts.cl/cartelera/sur-de-chile/arauco-chillan"
+
+MAIN_CINEPOLIS_CINEMAS = [
+    CINEHOYTS_VALPARAISO_URL,
+    CINEHOYTS_ARAUCO_ESTACION_URL,
+    CINEPOLIS_LA_REINA_URL,
+    CINEHOYTS_ARAUCO_MAIPU_URL,
+    CINEHOYTS_PUENTE_ALTO_URL,
+    CINEHOYTS_CHILLAN_URL,
+]
+
+CINEMARK_MALLPLAZA_ARICA = (
+    "https://www.cinemark.cl/cine?tag=2305&cine=cinemark_mallplaza_arica"
+)
+CINEMARK_MALLPLAZA_BIOBIO = (
+    "https://www.cinemark.cl/cine?tag=2302&cine=cinemark_mallplaza_mirador_bio_bio"
+)
+CINEMARK_ARAUCO_CORONEL = (
+    "https://www.cinemark.cl/cine?tag=2306&cine=cinemark_arauco_coronel"
+)
+CINEMARK_MALLPLAZA_IQUIQUE = (
+    "https://www.cinemark.cl/cine?tag=520&cine=cinemark_mallplaza_iquique"
+)
+CINEMARK_OPEN_LA_CALERA = (
+    "https://www.cinemark.cl/cine?tag=2308&cine=cinemark_open_la_calera"
+)
+CINEMARK_MALLPLAZA_LA_SERENA = (
+    "https://www.cinemark.cl/cine?tag=2309&cine=cinemark_mallplaza_la_serena"
+)
+CINEMARK_PORTAL_OSORNO = (
+    "https://www.cinemark.cl/cine?tag=2301&cine=cinemark_portal_osorno"
+)
+CINEMARK_OPEN_OVALLE = "https://www.cinemark.cl/cine?tag=2304&cine=cinemark_open_ovalle"
+CINEMARK_OPEN_RANCAGUA = (
+    "https://www.cinemark.cl/cine?tag=2303&cine=cinemark_open_rancagua"
+)
+CINEMARK_RANCAGUA = "https://www.cinemark.cl/cine?tag=517&cine=cinemark_rancagua"
+CINEMARK_ALTO_LAS_CONDES = (
+    "https://www.cinemark.cl/cine?tag=512&cine=cinemark_alto_las_condes"
+)
+CINEMARK_MALLPLAZA_NORTE = (
+    "https://www.cinemark.cl/cine?tag=572&cine=cinemark_mallplaza_norte"
+)
+CINEMARK_MALLPLAZA_OESTE = (
+    "https://www.cinemark.cl/cine?tag=513&cine=cinemark_mallplaza_oeste"
+)
+CINEMARK_MALLPLAZA_TOBALABA = (
+    "https://www.cinemark.cl/cine?tag=519&cine=cinemark_mallplaza_tobalaba"
+)
+CINEMARK_MALLPLAZA_VESPUCIO = (
+    "https://www.cinemark.cl/cine?tag=511&cine=cinemark_mallplaza_vespucio"
+)
+CINEMARK_MID_MALL_MAIPU = (
+    "https://www.cinemark.cl/cine?tag=2307&cine=cinemark_mid_mall_maipu"
+)
+CINEMARK_MALLPLAZA_TREBOL = (
+    "https://www.cinemark.cl/cine?tag=548&cine=cinemark_mallplaza_trebol"
+)
+CINEMARK_ESPACIO_URBANO = (
+    "https://www.cinemark.cl/cine?tag=514&cine=cinemark_espacio_urbano"
+)
+CINEMARK_MALL_MARINA = "https://www.cinemark.cl/cine?tag=570&cine=cinemark_mall_marina"
+CINEMARK_PORTAL_NUNOA_URL = (
+    "https://www.cinemark.cl/cine?tag=2300&cine=cinemark_portal_nunoa"
+)
+
+MAIN_CINEMARK_CINEMAS = [
+    CINEMARK_MALLPLAZA_ARICA,
+    CINEMARK_MALLPLAZA_BIOBIO,
+    CINEMARK_ARAUCO_CORONEL,
+    CINEMARK_MALLPLAZA_IQUIQUE,
+    CINEMARK_OPEN_LA_CALERA,
+    CINEMARK_MALLPLAZA_LA_SERENA,
+    CINEMARK_PORTAL_OSORNO,
+    CINEMARK_OPEN_OVALLE,
+    CINEMARK_OPEN_RANCAGUA,
+    CINEMARK_RANCAGUA,
+    CINEMARK_ALTO_LAS_CONDES,
+    CINEMARK_MALLPLAZA_NORTE,
+    CINEMARK_MALLPLAZA_OESTE,
+    CINEMARK_MALLPLAZA_TOBALABA,
+    CINEMARK_MALLPLAZA_VESPUCIO,
+    CINEMARK_MID_MALL_MAIPU,
+    CINEMARK_MALLPLAZA_TREBOL,
+    CINEMARK_ESPACIO_URBANO,
+    CINEMARK_MALL_MARINA,
+    CINEMARK_PORTAL_NUNOA_URL,
+]
