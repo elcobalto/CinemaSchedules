1032845d8698d083806685afdae2fbc9c9ef6928
Cristóbal Sepúlveda <crisep95@gmail.com>
2022-04-13 00:51:15 -0400

    feat: raw html scrapping (#1)

--- apps/cinemas/services.py
+++ apps/cinemas/services.py
@@ -1,9 +1,26 @@
-from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-HOYTS_LA_REINA_URL = 'https://cinehoyts.cl/cartelera/santiago-oriente/cinepolis-la-reina'
+CINEPOLIS_LA_REINA_URL = (
+    "https://cinehoyts.cl/cartelera/santiago-oriente/cinepolis-la-reina"
+)
+CINEMARK_ALTO_LAS_CONDES_URL = (
+    "https://www.cinemark.cl/cine?tag=2300&cine=cinemark_portal_nunoa"
+)
 
-def scrapp_cinehoyts_schedule(hoyts_link: str = HOYTS_LA_REINA_URL):
-    page = requests.get(hoyts_link)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    print(soup)
+
+def scrapp_cinehoyts_schedule(hoyts_link: str = CINEPOLIS_LA_REINA_URL):
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get(hoyts_link)
+    page = driver.page_source
+    raw_movie_list = page.split("originalTitle=")
+    movies = {}
+    for raw_movie in raw_movie_list[1:]:
+        movie = raw_movie.split("&")[0]
+        if movie in movies:
+            movies[movie] += 1
+        else:
+            movies[movie] = 1
+    print(movies)
+    driver.close()
+
