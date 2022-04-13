778f435bf5e343a50f3098bbd66ececa16bde030
Cristóbal Sepúlveda <crisep95@gmail.com>
2022-04-13 00:08:13 -0400

    feat: cinemark

--- apps/cinemas/services.py
+++ apps/cinemas/services.py
@@ -1,15 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 
-CINEPOLIS_LA_REINA_URL = 'https://cinehoyts.cl/cartelera/santiago-oriente/cinepolis-la-reina'
-CINEMARK_ALTO_LAS_CONDES_URL = 'https://www.cinemark.cl/cine?tag=2300&cine=cinemark_portal_nunoa'
+CINEPOLIS_LA_REINA_URL = (
+    "https://cinehoyts.cl/cartelera/santiago-oriente/cinepolis-la-reina"
+)
+CINEMARK_ALTO_LAS_CONDES_URL = (
+    "https://www.cinemark.cl/cine?tag=2300&cine=cinemark_portal_nunoa"
+)
+
 
 def scrapp_cinehoyts_schedule(hoyts_link: str = CINEPOLIS_LA_REINA_URL):
     page = requests.get(hoyts_link)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, "html.parser")
     print(soup)
+
 
 def scrapp_cinemark_schedule(hoyts_link: str = CINEMARK_ALTO_LAS_CONDES_URL):
     page = requests.get(hoyts_link)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, "html.parser")
     print(soup)
