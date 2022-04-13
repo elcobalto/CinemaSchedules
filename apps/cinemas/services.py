82713fb9a113cfbe92c9342bd188dcdb53da2f86
Cristóbal Sepúlveda <crisep95@gmail.com>
2022-04-13 00:49:15 -0400

    feat: scrapping html page

--- apps/cinemas/services.py
+++ apps/cinemas/services.py
@@ -12,6 +12,15 @@ CINEMARK_ALTO_LAS_CONDES_URL = (
 def scrapp_cinehoyts_schedule(hoyts_link: str = CINEPOLIS_LA_REINA_URL):
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(hoyts_link)
-    print(driver.page_source)
+    page = driver.page_source
+    raw_movie_list = page.split("originalTitle=")
+    movies = {}
+    for raw_movie in raw_movie_list[1:]:
+        movie = raw_movie.split("&")[0]
+        if movie in movies:
+            movies[movie] += 1
+        else:
+            movies[movie] = 1
+    print(movies)
     driver.close()
 
